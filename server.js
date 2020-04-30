dbddc19b3cf34e2a9b111c21d3873924f2967837
arthuranst <56081239+arthuranst@users.noreply.github.com>
2020-04-30 15:04:02 +0200

    update multijoueur

--- server.js
+++ server.js
@@ -4,51 +4,43 @@ const express = require('express');
 const socketIO = require('socket.io');
 
 const port = process.env.PORT || 3000;
-const index =  '/pictionary.html';
+const index = '/pictionary.html';
 
 const server = express()
-	.use((req, res) => res.sendFile(index, { root: __dirname}))
-	.listen(port, () => console.log('Server started on port ', port));
+    .use((req, res) => res.sendFile(index, { root: __dirname }))
+    .listen(port, () => console.log('Server started on port', port));
 
 const io = socketIO(server);
-let users =  [];                                  
+
+let users = [];
 
 io.on('connection', (socket) => {
-	console.log('A new user joined');
+    console.log('A new user joined the game');
 
-	onConnection(socket);
+    onConnection(socket);
 });
 
 function onConnection(socket) {
-	socket.on('username', (username) => {
-		console.log('Player name:', username);
-		socket.username = username;
-		users.push(socket);
-		sendUsers();
-	});
-
-	socket.on('disconnect', () => {
-		console.log('Player left the game');
-		users.filter(user => {
-			return user !== socket;
-		});
-		sendUsers();
-
-	});
-
-	socket.on('line', data => {
-		socket.broadcast.emit('line', data);
-	});
+    socket.on('username', (username) => {
+        console.log('Player name :', username);
+        socket.username = username;
+        users.push(socket);
+        sendUsers();
+    });
+    socket.on('disconnect', (username) => {
+        console.log('A user left the game');
+        users = users.filter(user => {
+            return user !== socket;
+        });
+        sendUsers();
+    });
+    socket.on('line', data => {
+        socket.broadcast.emit('line', data);
+    });
 }
 
 function sendUsers () {
-	io.emit('users', users.map(usr => {
-		return user.username;
-	}));
+    io.emit('users', users.map(user => {
+        return user.username;
+    }));
 }
-
-
-
-
-
-
